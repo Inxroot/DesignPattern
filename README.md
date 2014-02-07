b223b935c0cf0b629d2f24f2231a90ae3c5b7aa2
anand <inxroot.asingh07@gmail.com>
2014-02-07 19:04:59 +0530

    design pattern added

---
+++ README.md
@@ -0,0 +1,3 @@
+# version 1.0
+
+This is the singleton design pattern demo.
---
+++ src/com/anand/test/SingletonObject.java
@@ -0,0 +1,13 @@
+package com.anand.test;
+
+public class SingletonObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Singleton object = Singleton.getInstance();
+		object.showMessage();
+				
+		
+	}
+
+}
